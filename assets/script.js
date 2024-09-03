769d83f6a5dc876b98cfdb34dd05574902178122
sLokesh-code <lokesh.1512@hotmail.com>
2024-09-03 12:55:01 +0530

    added view count feature

--- assets/script.js
+++ assets/script.js
@@ -49,3 +49,14 @@ $tabBtn.forEach(item => {
     });
 });
 
+
+// View Count
+
+const counter = document.querySelector(".counter-number");//links to element in HTML file
+async function updateCounter(){
+	let response = await fetch("https://vujyid2ixj25vcrjyjyhmdxyjy0rieas.lambda-url.ap-south-1.on.aws/");//lamda's function URL.
+	let count = await response.json();
+	counter.innerHTML = count;
+}
+
+updateCounter();
